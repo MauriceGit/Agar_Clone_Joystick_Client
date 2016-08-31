938a69298b20b75b3db81216735ecee29cfd40f2
mauricegit <maurice.tollmien@gmail.com>
2016-08-31 11:21:39 +0200

    added Makefile

---
+++ Makefile
@@ -0,0 +1,38 @@
+
+TARGET = agar_joystick
+
+SRCS_C = vector quaternions joystick hmd joystickCamera sphere logic
+SRCS_CPP = agar_hmd tinythread/tinythread easywsclient/easywsclient
+
+OBJS_C = $(SRCS_C:%=%.o)
+OBJS_CPP = $(SRCS_CPP:%=%.o)
+
+CC = g++
+LD = g++
+
+CC_FLAGS = -std=c++11 -I./ -c -o
+LD_FLAGS = -L/usr/lib/x86_64-linux-gnu/
+LD_LIBS  = `pkg-config glfw3 --static --cflags --libs` -lGLU -lGL -lGLEW -lpthread -lssl -lcrypto
+
+.PHONY: all
+
+# TARGETS
+all: $(TARGET)
+
+$(TARGET): clean compile link
+
+clean:
+	rm -f $(TARGET)
+	rm -f $(OBJS_C)
+	rm -f $(OBJS_CPP)
+
+compile:
+	@echo -n "compiling source files ... "
+	@$(foreach SRC, $(SRCS_C),   ( $(CC) $(CC_FLAGS) $(SRC).o $(SRC).c   -W -O3 -Wno-write-strings );)
+	@$(foreach SRC, $(SRCS_CPP), ( $(CC) $(CC_FLAGS) $(SRC).o $(SRC).cpp -W -O3 -Wno-write-strings );)
+	@echo "done"
+
+link:
+	@echo -n "linking object files ... "
+	$(LD) $(LD_FLAGS) -o $(TARGET) $(OBJS_C) $(OBJS_CPP) $(LD_LIBS)
+	@echo "done"
--- compile.sh
+++ compile.sh
@@ -8,27 +8,27 @@ echo "--> removing *.o, $programName .."
 rm -f *.o "$programName"
 
 echo "--> compile joystick modules .."
-g++-4.9 -std=c++11 -I./ -c -o vector.o vector.c -W -O3
-g++-4.9 -std=c++11 -I./ -c -o quaternions.o quaternions.c -W -O3
-g++-4.9 -std=c++11 -I./ -c -o joystick.o joystick.c -W -O3
-g++-4.9 -std=c++11 -I./ -c -o hmd.o hmd.c -W -O3
-g++-4.9 -std=c++11 -I./ -c -o joystickCamera.o joystickCamera.c -W -O3
+g++ -std=c++11 -I./ -c -o vector.o vector.c -W -O3
+g++ -std=c++11 -I./ -c -o quaternions.o quaternions.c -W -O3
+g++ -std=c++11 -I./ -c -o joystick.o joystick.c -W -O3
+g++ -std=c++11 -I./ -c -o hmd.o hmd.c -W -O3
+g++ -std=c++11 -I./ -c -o joystickCamera.o joystickCamera.c -W -O3
 
 echo "--> compile helper modules .."
-g++-4.9 -std=c++11 -I./ -c -o sphere.o sphere.c -W -O3
-g++-4.9 -std=c++11 -I./ -c -o logic.o logic.c -W -O3
+g++ -std=c++11 -I./ -c -o sphere.o sphere.c -W -O3
+g++ -std=c++11 -I./ -c -o logic.o logic.c -W -O3
 
 echo "--> compile program .."
-g++-4.9 -std=c++11 -I./ -c -o agar_hmd.o agar_hmd.cpp -Wno-write-strings -W -O3
+g++ -std=c++11 -I./ -c -o agar_hmd.o agar_hmd.cpp -Wno-write-strings -W -O3
 
 echo "--> compile tinythread .."
-g++-4.9 -std=c++11 -I./ -c -o "$threadPrefix"tinythread.o "$threadPrefix"tinythread.cpp -W -O3
+g++ -std=c++11 -I./ -c -o "$threadPrefix"tinythread.o "$threadPrefix"tinythread.cpp -W -O3
 
 echo "--> compile easywsclient .."
-g++-4.9 -std=c++11 -I./ -c -o "$wsPrefix"easywsclient.o "$wsPrefix"easywsclient.cpp -W -O3
+g++ -std=c++11 -I./ -c -o "$wsPrefix"easywsclient.o "$wsPrefix"easywsclient.cpp -W -O3
 
 echo "--> linking everything .."
-g++-4.9 -L/usr/lib/x86_64-linux-gnu/ -o "$programName" quaternions.o joystick.o hmd.o joystickCamera.o sphere.o vector.o logic.o agar_hmd.o "$threadPrefix"tinythread.o "$wsPrefix"easywsclient.o `pkg-config glfw3 --static --cflags --libs` -lGLU -lGL -lGLEW -lpthread -lssl -lcrypto
+g++ -L/usr/lib/x86_64-linux-gnu/ -o "$programName" quaternions.o joystick.o hmd.o joystickCamera.o sphere.o vector.o logic.o agar_hmd.o "$threadPrefix"tinythread.o "$wsPrefix"easywsclient.o `pkg-config glfw3 --static --cflags --libs` -lGLU -lGL -lGLEW -lpthread -lssl -lcrypto
 
 rm -f *.o
 rm -f "$wsPrefix"*.o
